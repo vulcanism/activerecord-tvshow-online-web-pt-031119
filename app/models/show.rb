b2f4b7796a91f285b2e450fff54e30827a982fcf
Savannah C <sgcorrell@gmail.com>
2019-05-30 19:08:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,10 @@
 class Show < ActiveRecord::Base
   
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
   
-  def most_popular_show
+  def self.most_popular_show
     Show.find_by(rating: highest_rating)
   end
   
