3059548ea93239faa527d4b46120be30a915a821
Savannah C <sgcorrell@gmail.com>
2019-05-30 19:01:59 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,6 +2,9 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end
